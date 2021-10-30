4c312fa4ea1020cae6a18351c09fa7e2f8392186
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-10-30 19:07:45 -0300

    some refactor

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./index.module.scss";
 
 interface ICardProps {
   link?: string;
@@ -16,11 +17,11 @@ export const Card: React.FC<ICardProps> = ({
 }) => {
   const Wrapper = (children: React.ReactElement) =>
     isLink ? (
-      <a href={link} className={className}>
+      <a href={link} className={styles.card}>
         {children}
       </a>
     ) : (
-      <div className={className}>{children}</div>
+      <div className={styles.card}>{children}</div>
     );
   return Wrapper(
     <>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { Card, FlipSpinner } from "components";
+import styles from "styles/Home.module.scss";
+import { Card } from "components";
 
 const Home: NextPage = () => {
   return (
